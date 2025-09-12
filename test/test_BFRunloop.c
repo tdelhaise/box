94a5cad1179f2047a7b9dc39e68323baab34fdb2
Thierry DELHAISE <thierry.delhaise@gmail.com>
2025-09-12 11:43:02 +0200

    test(style): descriptive local names in test_BFRunloop (no abbreviated locals)

--- test/test_BFRunloop.c
+++ test/test_BFRunloop.c
@@ -8,33 +8,33 @@ typedef struct CounterCtx {
     int count;
 } CounterCtx;
 
-static void handler(BFRunloop *rl, BFRunloopEvent *ev, void *ctx) {
-    (void)rl;
-    CounterCtx *c = (CounterCtx *)ctx;
-    if (ev->type != BFRunloopEventStop) {
-        c->count++;
+static void handler(BFRunloop *runloop, BFRunloopEvent *event, void *context) {
+    (void)runloop;
+    CounterCtx *counter = (CounterCtx *)context;
+    if (event->type != BFRunloopEventStop) {
+        counter->count++;
     }
 }
 
 int main(void) {
-    BFRunloop *rl = BFRunloopCreate();
-    assert(rl != NULL);
-    CounterCtx ctx = {0};
-    assert(BFRunloopSetHandler(rl, handler, &ctx) == 0);
-    assert(BFRunloopStart(rl) == 0);
+    BFRunloop *runloop = BFRunloopCreate();
+    assert(runloop != NULL);
+    CounterCtx context = {0};
+    assert(BFRunloopSetHandler(runloop, handler, &context) == 0);
+    assert(BFRunloopStart(runloop) == 0);
 
-    for (int i = 0; i < 10; ++i) {
-        BFRunloopEvent ev;
-        memset(&ev, 0, sizeof(ev));
-        ev.type = 100;
-        assert(BFRunloopPost(rl, &ev) == 0);
+    for (int index = 0; index < 10; ++index) {
+        BFRunloopEvent event;
+        memset(&event, 0, sizeof(event));
+        event.type = 100;
+        assert(BFRunloopPost(runloop, &event) == 0);
     }
 
-    BFRunloopPostStop(rl);
-    BFRunloopJoin(rl);
-    BFRunloopFree(rl);
+    BFRunloopPostStop(runloop);
+    BFRunloopJoin(runloop);
+    BFRunloopFree(runloop);
 
-    assert(ctx.count == 10);
+    assert(context.count == 10);
     printf("test_BFRunloop: OK\n");
     return 0;
 }
