7bcb042e50a6286b050a9dc224a7423a09c57b02
Thierry DELHAISE <thierry.delhaise@gmail.com>
2025-09-17 11:28:44 +0200

    Create BFFileManager

---
+++ sources/include/BFFileManager.h
@@ -0,0 +1,24 @@
+//
+//  BFFileManager.h
+//  box
+//
+//  Created by Thierry DELHAISE on 16/09/2025.
+//
+
+#ifndef BF_FILE_MANAGER_H
+#define BF_FILE_MANAGER_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+typedef struct BFFile BFFile;
+	
+#ifdef __cplusplus
+}
+#endif
+	
+#endif // !BF_FILE_MANAGER_H
---
+++ sources/lib/BFFileManager.c
@@ -0,0 +1,12 @@
+//
+//  BFFileManager.c
+//  BoxFoundation
+//
+//  Created by Thierry DELHAISE on 16/09/2025.
+//
+
+#include "BFFileManager.h"
+
+#include <stdio.h>
+
+
