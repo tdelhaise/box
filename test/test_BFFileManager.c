b7a1c698514805c2a4050d26a9d659c9e68bd284
Thierry DELHAISE <thierry.delhaise@gmail.com>
2025-10-17 00:38:29 +0200

    Guard PATH_MAX usage in legacy tests

--- test/test_BFFileManager.c
+++ test/test_BFFileManager.c
@@ -5,6 +5,9 @@
 
 #include <assert.h>
 #include <limits.h>
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
 #include <stdio.h>
 #include <string.h>
 #if defined(_WIN32)
--- test/test_BFStorageManager.c
+++ test/test_BFStorageManager.c
@@ -6,6 +6,9 @@
 
 #include <assert.h>
 #include <limits.h>
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
 #include <stdio.h>
 #include <string.h>
 
