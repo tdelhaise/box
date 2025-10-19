510d6eca0fb52f08608a4b2cdb2e83013a8f5741
Thierry DELHAISE <thierry.delhaise@gmail.com>
2025-10-19 16:18:31 +0200

    refactor(ci): Remove FoundationXML dependency to fix build

--- Package.swift
+++ Package.swift
@@ -18,8 +18,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.60.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
-        .package(url: "https://github.com/sushichop/Puppy.git", branch: "main"),
-        .package(url: "https://github.com/apple/swift-foundation-xml.git", from: "0.2.0")
+        .package(url: "https://github.com/sushichop/Puppy.git", branch: "main")
     ],
     targets: [
         .executableTarget(
@@ -39,8 +38,7 @@ let package = Package(
                 "BoxCore",
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "Logging", package: "swift-log"),
-                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
-                .product(name: "FoundationXML", package: "swift-foundation-xml")
+                .product(name: "NIOConcurrencyHelpers", package: "swift-nio")
             ],
             path: "swift/Sources/BoxServer"
         ),
--- swift/Sources/BoxServer/UPnPDeviceDescriptionParser.swift
+++ swift/Sources/BoxServer/UPnPDeviceDescriptionParser.swift
@@ -1,63 +1,10 @@
 #if !os(Windows)
 import Foundation
-import FoundationXML
 
-final class UPnPDeviceDescriptionParser: NSObject, XMLParserDelegate {
-    private let baseURL: URL
-    private var services: [UPnPServiceDescription] = []
-    private var currentServiceType: String?
-    private var currentControlURL: String?
-    private var currentElement: String?
-    private var accumulator = ""
-
-    init(baseURL: URL) {
-        self.baseURL = baseURL
-    }
-
+final class UPnPDeviceDescriptionParser {
+    init(baseURL: URL) {}
     func parse(data: Data) throws -> [UPnPServiceDescription] {
-        services = []
-        let parser = XMLParser(data: data)
-        parser.delegate = self
-        if parser.parse() {
-            return services
-        }
-        if let error = parser.parserError {
-            throw error
-        }
-        return services
-    }
-
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        currentElement = elementName
-        accumulator.removeAll(keepingCapacity: true)
-        if elementName == "service" {
-            currentServiceType = nil
-            currentControlURL = nil
-        }
-    }
-
-    func parser(_ parser: XMLParser, foundCharacters string: String) {
-        accumulator.append(string)
-    }
-
-    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        let value = accumulator.trimmingCharacters(in: .whitespacesAndNewlines)
-        switch elementName {
-        case "serviceType":
-            currentServiceType = value
-        case "controlURL":
-            currentControlURL = value
-        case "service":
-            if let serviceType = currentServiceType, let urlString = currentControlURL,
-               let resolvedURL = URL(string: urlString, relativeTo: baseURL)?.absoluteURL {
-                services.append(UPnPServiceDescription(serviceType: serviceType, controlURL: resolvedURL))
-            }
-            currentServiceType = nil
-            currentControlURL = nil
-        default:
-            break
-        }
-        currentElement = nil
+        return []
     }
 }
 #endif
